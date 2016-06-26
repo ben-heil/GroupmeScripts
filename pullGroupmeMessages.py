25f5402c206564a75337dc7c3d988aadc78cc1a4
Ben Heil <ben_heil@baylor.edu>
2016-06-26 18:06:32 -0400

    Learning to deal with encoding

--- pullGroupmeMessages.py
+++ pullGroupmeMessages.py
@@ -8,14 +8,23 @@ import argparse
 #Use argparse to take in the user's API token from command line
 parser = argparse.ArgumentParser(description="add flavor text here")
 parser.add_argument("token", help="Your GroupMe API token (go to dev.groupme.com for information on how to get one")
+parser.add_argument("--outFile", default="out.txt", help="Results go here")
+
 args = parser.parse_args()
-parser.add_argument("outFile", help="Results go here", default="out.txt")
 
 baseURL = "https://api.groupme.com/v3"
 
+#Retrieve and parse the response
 groupRequest = requests.get(baseURL + "/groups?token=" + args.token)
 
-print(groupRequest.encoding)
+try:
+    outFile = open(args.outFile, 'w')
+except IOError:
+    print("The file didn't open")
+    exit(1)
+
+#Have to encode the string before writing 
+outFile.write(str(groupRequest.text.encode('utf-8')))
 
 #print("Input the ID of the group which you would like to download messages from)
 #
