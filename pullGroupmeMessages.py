c2d4edf4b876e6877cedc246bc99b93bef512c31
Ben Heil <ben_heil@baylor.edu>
2016-06-26 16:24:02 -0400

    Learning how to use requests library

--- pullGroupmeMessages.py
+++ pullGroupmeMessages.py
@@ -1,13 +1,27 @@
-#!/Python34/python.exe
+#!/usr/bin/python
 
 import sys
 import json
 import requests
+import argparse
 
-#Take in the API token as a command line parameter
-token = sys.argv[1:]
-groupId = sys.argv[2:]
+#Use argparse to take in the user's API token from command line
+parser = argparse.ArgumentParser(description="add flavor text here")
+parser.add_argument("token", help="Your GroupMe API token (go to dev.groupme.com for information on how to get one")
+args = parser.parse_args()
+parser.add_argument("outFile", help="Results go here", default="out.txt")
 
 baseURL = "https://api.groupme.com/v3"
 
-print(baseURL)
+groupRequest = requests.get(baseURL + "/groups?token=" + args.token)
+
+print(groupRequest.encoding)
+
+#print("Input the ID of the group which you would like to download messages from)
+#
+#groupID = str(sys.stdin.read())
+#
+#urlExtension = "/groups/" + groupId + "/messages?" + args.token
+#
+#print(baseURL + urlExtension)
+#
