d0f79fc53909c164fcd428956366cd95a5eda55a
Ben Heil <ben_heil@baylor.edu>
2016-06-27 20:08:02 -0400

    Figuring out json parsing

--- pullGroupmeMessages.py
+++ pullGroupmeMessages.py
@@ -15,7 +15,7 @@ args = parser.parse_args()
 baseURL = "https://api.groupme.com/v3"
 
 #Retrieve and parse the response
-groupRequest = requests.get(baseURL + "/groups?token=" + args.token)
+groupResponse = requests.get(baseURL + "/groups?token=" + args.token)
 
 try:
     outFile = open(args.outFile, 'w')
@@ -23,8 +23,16 @@ except IOError:
     print("The file didn't open")
     exit(1)
 
+jsonDict = groupResponse.json()
+
+groups = jsonDict["response"]
+for item in groups:
+    print(item["name"])
+#print(json.dumps(jsonDict, sort_keys = True, indent=4))
+
+
 #Have to encode the string before writing 
-outFile.write(str(groupRequest.text.encode('utf-8')))
+outFile.write(str(groupResponse.text.encode('utf-8')))
 
 #print("Input the ID of the group which you would like to download messages from)
 #
